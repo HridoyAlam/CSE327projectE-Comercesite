97bb008a9893217fe357c97fdb4b24584f2b7f08
HridoyAlam <optimus5289@gmail.com>
2020-09-23 18:41:28 +0600

    difeerent category slide added

--- eab/shop/views.py
+++ eab/shop/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
+from django.http import HttpResponse
+
 def index(request):
-    return render(request, 'shop/index.html')
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
-    return render(request,'shop/about.html')
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse("We are at contact")
@@ -21,5 +42,4 @@ def productView(request):
     return HttpResponse("We are at product view")
 
 def checkout(request):
-    return render(request,'shop/checkout.html')
-    #return HttpResponse("We are at checkout")
+    return HttpResponse("We are at checkout")
