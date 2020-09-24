7c308da8f4be1dc44e6968a0c4fe724beac973dc
HridoyAlam <optimus5289@gmail.com>
2020-09-24 23:41:55 +0600

    Tracker update
    Now Customer can update their orders

--- eab/eab/settings.py
+++ eab/eab/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'eab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['eab/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
